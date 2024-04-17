6be2230355ed85a4532c68eff0fffdc75f68d22d
unknown <therealtiti4@gmail.com>
2024-04-17 10:26:40 +0000

    new

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+import { RouterLinkWithHref } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLinkWithHref, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
 })
 export class HomePage {
-  constructor() {}
+
+  myStatus:string="";
+
+  constructor(private storage:Storage) { }
+
+  async ionViewWillEnter(){
+    await this.storage.create();
+
+    this.myStatus = await this.storage.get('status');
+  }
 }
